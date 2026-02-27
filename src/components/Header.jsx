cebf7ef6b954e9cec409c5f04a430cfc26b4e446
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-02-27 15:50:54 +0530

    v1

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-4 bg-linear-to-b from black">
+      <img
+        className="w-40"
+        src="https://toppng.com/uploads/preview/netflix-logo-11609370106okqwb3asfk.png"
+      />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          className="scale-110"
+          src="src/assets/Netflix-bg.jpg"
+          alt="Netflix-bg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
